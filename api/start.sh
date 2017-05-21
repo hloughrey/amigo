c4f11f8c8ef8eb3e17d6fe222410d61cbcc30ac4
Hugh Loughrey <hugh.loughrey@gmail.com>
2017-05-21 10:13:26 +0100

    Add ability to switch environments

--- api/start.sh
+++ api/start.sh
@@ -2,4 +2,17 @@
 
 cd /var/www/amigo/api
 npm install
-pm2 start -x $APP --name="amigo-api" --no-daemon --watch
+
+if [ $NODE_ENV == "PROD" ]
+then
+    echo 'RUNNING PROD ENVIRONMENT'
+    npm run start
+elif [ $NODE_ENV == "DEV"  ]
+then
+    echo 'RUNNING DEV ENVIRONMENT'
+    npm run dev
+else
+    echo 'RUNNING TEST ENVIRONMENT'
+    ash -c "sleep 5; npm run test"
+fi
+
